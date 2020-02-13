0aba55977b12cad2d58ce94c83cc2d37e28cd64a
Balukuu <edgarbaluku@gmail.com>
2020-02-13 14:54:08 +0300

    Practice example

--- app/src/main/java/com/example/baluku/MainActivity.java
+++ app/src/main/java/com/example/baluku/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.baluku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sendMessage(View view) {
+        EditText message = findViewById(R.id.EditText);
+        Intent intent = new Intent(this, DisplayMessageActivty.class);
+        intent.putExtra("MESSAGE", message.getText().toString());
+
+        startActivity(intent);
+
+        message.setText("");
+    }
+
 }
