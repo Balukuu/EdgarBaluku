b15c0f5993f9444b0346511dce2374e033212a6f
EdgarBaluku <edgarbaluku@gmail.com>
2020-12-15 13:48:23 +0300

    internal and external strorage

---
+++ app/src/main/java/com/example/baluku/InternalExternal.java
@@ -0,0 +1,164 @@
+package com.example.baluku;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class InternalExternal extends AppCompatActivity {
+
+    EditText inputText, internal, external;
+
+    Button saveButton,readButton;
+
+    private String InternalFileName= "SampleAppInternalData.txt";
+    private String InternalFilePath = "MyFileStorage";
+
+    private String ExternalFileName = "StoredData.txt";
+    private String ExternalFilePath = "MyFileStorage";
+
+    File myInternalFile;
+    File myExternalFile;
+
+    String myData = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_internal_external);
+
+        inputText = findViewById(R.id.data);
+        internal = findViewById(R.id.stored_messages);
+        external = findViewById(R.id.external_stored_messages);
+
+        saveButton = findViewById(R.id.saveButton);
+        readButton = findViewById(R.id.readButton);
+
+        // Retrieving Files from the internal storage directory
+        myInternalFile = new File(getFilesDir(), InternalFileName);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeToInternal();
+                writeToExternal();
+            }
+        });
+
+        readButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               readFromExternal();
+               readFromInternal();
+            }
+        });
+
+        // Checking whether the external storage media is mounted
+        if (!isExternalStorageAvailable() || isExternalStorageReadOnly()) {
+            saveButton.setEnabled(false);
+        }
+        else {
+            myExternalFile = new File(getExternalFilesDir(ExternalFilePath), ExternalFileName);
+        }
+    }
+
+    private void writeToInternal() {
+
+        FileOutputStream fileOutputStream;
+
+        try {
+            fileOutputStream = openFileOutput(InternalFileName, Context.MODE_APPEND);
+            fileOutputStream.write(inputText.getText().toString().getBytes());
+            fileOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeToExternal() {
+        try {
+            FileOutputStream fos = new FileOutputStream(myExternalFile);
+            fos.write(inputText.getText().toString().getBytes());
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        inputText.setText("");
+        Toast.makeText(InternalExternal.this,
+                "StoredData.txt saved to External Storage...",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    private void readFromInternal() {
+
+        try {
+            FileInputStream fis = new FileInputStream(myInternalFile);
+            DataInputStream in = new DataInputStream(fis);
+            BufferedReader br =
+                    new BufferedReader(new InputStreamReader(in));
+            String strLine;
+            while ((strLine = br.readLine()) != null) {
+                myData = myData + strLine;
+            }
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        internal.setText(myData);
+
+    }
+
+    private void readFromExternal(){
+        try {
+            FileInputStream fis = new FileInputStream(myExternalFile);
+            DataInputStream in = new DataInputStream(fis);
+            BufferedReader br =
+                    new BufferedReader(new InputStreamReader(in));
+            String strLine;
+            while ((strLine = br.readLine()) != null) {
+                myData = myData + strLine;
+            }
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        external.setText(myData);
+
+        Toast.makeText(this,
+                "StoredData.txt data retrieved from External Storage... ",
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+    private static boolean isExternalStorageReadOnly() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isExternalStorageAvailable() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
+
+}
--- app/src/main/java/com/example/baluku/MainActivity.java
+++ app/src/main/java/com/example/baluku/MainActivity.java
@@ -7,7 +7,9 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -18,7 +20,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+   private static final int REQUEST_CALL = 1;
 
     BroadcastReceiver r = new BroadcastReceiver() {
 
@@ -28,25 +30,85 @@ public class MainActivity extends AppCompatActivity {
             ProgressBar bar = (ProgressBar) findViewById(R.id.progressBar);
             bar.setProgress(battery);
             TextView textView = (TextView) findViewById(R.id.textField);
-            textView.setText("THE BATTERY LEVEL :" + Integer.toString(battery) + "%");
+            String battery_level = "BATTERY LEVEL : " +
+                    battery +
+                    "%";
 
+            //String battery_level = String.format("%f", battery);
+            textView.setText(battery_level);
         }
     };
+
+    // AN INNER CLASS FOR THE BROADCAST RECEIVER .
+     /*
+     class Receiver extends BroadcastReceiver{
+       @Override
+        public void onReceive(Context context, Intent intent) {
+        }
+     }
+      */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registerReceiver(r, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+    }
+
+
+
+    public void clickMe(View view) {
+        EditText startAlarm = findViewById(R.id.startAlarm);
+        String counter_time = startAlarm.getText().toString();
+
+        if (!counter_time.isEmpty()) {
+
+            int time = Integer.parseInt(counter_time);
+
+            // CREATING THE INTENT AN D
+            Intent intent = new Intent(this, BroadcastReciver.class);
+
+            //creating a pending intent
+            PendingIntent pendingIntent = PendingIntent.getBroadcast
+                    (this.getApplicationContext(), 0, intent, 0);
+
+            //real time clock
+            AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+            //CALLING THE ALARM
+            alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() +
+                    (time * 1000), pendingIntent);
+
+            // TOAST TO DISPLAY THE ALARM TIME
+            Toast.makeText(this, "Alarm set in" + time + "seconds", Toast.LENGTH_LONG).show();
+        } else {
+            Toast toast = Toast.makeText(this, "PLEASE ENTER THE COUNTER TIME ! ",
+                    Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER, 0, 220);
+            toast.show();
+            startAlarm.requestFocus();
+        }
     }
 
     public void sendMessage(View view) {
-        EditText message = findViewById(R.id.EditText);
-        Intent intent = new Intent(this, DisplayMessageActivty.class);
-        intent.putExtra("MESSAGE", message.getText().toString());
+        EditText message = findViewById(R.id.message);
+        String msg = message.getText().toString();
 
-        startActivity(intent);
+        if (!msg.isEmpty()) {
+            Intent intent = new Intent(this, DisplayMessageActivty.class);
+            intent.putExtra("MESSAGE", msg);
+            startActivity(intent);
 
-        message.setText("");
+            message.setText("");
+
+        } else {
+            Toast mytoast = Toast.makeText(this, "PLEASE ENTER A MESSAGE TO SEND ! ", Toast.LENGTH_SHORT);
+            mytoast.setGravity(Gravity.CENTER, 0, -600);
+            mytoast.show();
+            message.requestFocus();
+        }
+
     }
 
     public boolean onCreateOptionsMenu (Menu menu){
@@ -70,33 +132,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(this,  ListViewActivity.class));
 
                 return true;
+            case R.id.InternalExternal:
+                startActivity(new Intent(this,  InternalExternal.class));
+
+                return true;
         default:
             return super.onOptionsItemSelected(item);
         }
 
     }
-    public void clickMe(View view) {
-        EditText startAlarm = findViewById(R.id.time);
-        int time = Integer.parseInt(startAlarm.getText().toString());
 
-        // CREATING THE INTENT AN D
-        Intent intent = new Intent(this, BroadcastReceiver.class);
 
-        //creating a pending intent
-        PendingIntent pendingIntent = PendingIntent.getBroadcast
-                (this.getApplicationContext(),0,intent,0);
 
-        //real time clock
-        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-
-        //CALLING THE ALARM
-        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+(time* 1000),
-                pendingIntent);
-
-        // TOAST TO DISPLAY THE ALARM TIME
-
-        Toast.makeText(this, "Alarm set in"+time+"seconds", Toast.LENGTH_LONG).show();
 
     }
 
-}
